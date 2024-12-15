69a201dfa7a90777e958db517f9c213f375e3db2
Gourav <gouravzutshi@gmail.com>
2024-12-15 17:34:35 +0530

    Added encoder testbench file to main branch

---
+++ tb_encoder_4_to_2.v
@@ -0,0 +1,27 @@
+module tb_encoder_4_to_2;
+
+    reg [3:0] in;
+    reg [1:0] out;
+
+    encoder4_to_2 uut(
+
+        .in(in),
+        .out(out)
+
+    );
+
+    initial begin
+
+        $monitor("Time=%0t | in=%b, out=%b",$time,in,out);
+
+        in=4'b0001;#10;
+        in=4'b0010;#10;
+        in=4'b0100;#10;
+        in=4'b1000;#10;
+
+        #finish;
+
+    end
+
+
+endmodule
