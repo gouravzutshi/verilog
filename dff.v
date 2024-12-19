821cc395c10a5377f35280cf30486e7402a8fa7a
Gourav <gouravzutshi@gmail.com>
2024-12-19 17:53:31 +0530

    Test message

--- dff.v
+++ dff.v
@@ -2,10 +2,14 @@
 module dff(
     input wire d,
     input wire clk,
+    input reset,
     output reg q
 );
 
-always@(posedge clk) begin
+always@(posedge clk or posedge reset) begin
+if(reset)
+q<=1b'0;
+else
 q<=d;
 end
 
